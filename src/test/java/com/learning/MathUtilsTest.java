6d0c7e70b83a1f37926235c32f3fd719cc9a8fe8
Bhat <Chinmay.Bhat@philips.com>
2020-05-23 01:55:41 +0530

    Conditional executions and assumptions

--- src/test/java/com/learning/MathUtilsTest.java
+++ src/test/java/com/learning/MathUtilsTest.java
@@ -2,11 +2,12 @@ package com.learning;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 
 class MathUtilsTest {
 
@@ -18,7 +19,7 @@ class MathUtilsTest {
 	}
 
 	@Test
-	@DisplayName("Testing add two numbers method")
+	@EnabledOnOs(OS.MAC)//Does not execute the test if pre-condition is not met
 	void testAdd() {
 		int result = mathUtils.add(10, 20);
 		assertEquals(30, result, "The add method should add two numbers");
@@ -30,10 +31,24 @@ class MathUtilsTest {
 	}
 
 	@Test
-	@Disabled//Skips the Test 
 	void testCalculateAreaOfCircle() {
+		int radius = 0;
+		boolean isValidRadius = validateRadius(radius);
+		//Runs the test but skips if the condition is false
+		assumeTrue(isValidRadius, "The radius should be a positive integer");
 		double result = mathUtils.calculateAreaOfCircle(10);
 		assertEquals(314.1592653589793, result, "The calculateAreaOfCircle method should find area of a circle");
+	}
+
+	private boolean validateRadius(int radius) {
+
+		boolean isValid = false;
+
+		if (radius > 0) {
+			isValid = true;
+		}
+
+		return isValid;
 	}
 
 }
