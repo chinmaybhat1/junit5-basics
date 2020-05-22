b82efe55eb102a0876ea42e2187e19c4cb484c76
Bhat <Chinmay.Bhat@philips.com>
2020-05-23 00:28:43 +0530

    Asserting exceptions with assertThrows

--- src/main/java/com/learning/MathUtils.java
+++ src/main/java/com/learning/MathUtils.java
@@ -6,6 +6,18 @@ public class MathUtils {
 		return number1 + number2;
 	}
 
+	public int subtract(int number1, int number2) {
+		return number1 - number2;
+	}
+
+	public int multiply(int number1, int number2) {
+		return number1 * number2;
+	}
+
+	public int divide(int number1, int number2) {
+		return number1 / number2;
+	}
+
 	public double calculateAreaOfCircle(int radius) {
 		return Math.PI * radius * radius;
 	}
--- src/test/java/com/learning/MathUtilsTest.java
+++ src/test/java/com/learning/MathUtilsTest.java
@@ -1,6 +1,7 @@
 package com.learning;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -11,8 +12,14 @@ class MathUtilsTest {
 
 		MathUtils mathUtils = new MathUtils();
 		int result = mathUtils.add(10, 20);
-
 		assertEquals(30, result, "The add method should add two numbers");
+	}
+
+	@Test
+	void testDivide() {
+
+		MathUtils mathUtils = new MathUtils();
+		assertThrows(NullPointerException.class, () -> mathUtils.divide(10, 0), "Divide by zero should throw");
 	}
 
 	@Test
@@ -20,7 +27,6 @@ class MathUtilsTest {
 
 		MathUtils mathUtils = new MathUtils();
 		double result = mathUtils.calculateAreaOfCircle(10);
-
 		assertEquals(314.1592653589793, result, "The calculateAreaOfCircle method should find area of a circle");
 	}
 
