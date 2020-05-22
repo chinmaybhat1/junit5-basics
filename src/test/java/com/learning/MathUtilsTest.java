9ac2f21e5d3f628bbdc959c7e174d8a6c61db5c2
Bhat <Chinmay.Bhat@philips.com>
2020-05-23 00:54:48 +0530

    Lifecycle hooks: @BeforeAll, @BeforeEach, @AfterEach

--- src/test/java/com/learning/MathUtilsTest.java
+++ src/test/java/com/learning/MathUtilsTest.java
@@ -3,29 +3,43 @@ package com.learning;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class MathUtilsTest {
 
+	MathUtils mathUtils;
+
+	@BeforeAll
+	static void beforeAllInit() {
+		System.out.println("Initialising before all");
+	}
+
+	@BeforeEach
+	void init() {
+		mathUtils = new MathUtils();
+	}
+
+	@AfterEach
+	void cleanup() {
+		System.out.println("Performing cleanup");
+	}
+
 	@Test
 	void testAdd() {
-
-		MathUtils mathUtils = new MathUtils();
 		int result = mathUtils.add(10, 20);
 		assertEquals(30, result, "The add method should add two numbers");
 	}
 
 	@Test
 	void testDivide() {
-
-		MathUtils mathUtils = new MathUtils();
-		assertThrows(NullPointerException.class, () -> mathUtils.divide(10, 0), "Divide by zero should throw");
+		assertThrows(ArithmeticException.class, () -> mathUtils.divide(10, 0), "Divide by zero should throw");
 	}
 
 	@Test
 	void testCalculateAreaOfCircle() {
-
-		MathUtils mathUtils = new MathUtils();
 		double result = mathUtils.calculateAreaOfCircle(10);
 		assertEquals(314.1592653589793, result, "The calculateAreaOfCircle method should find area of a circle");
 	}
