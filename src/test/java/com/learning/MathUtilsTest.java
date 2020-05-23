4cd74843e905626820e1356720899d71c523110e
Bhat <Chinmay.Bhat@philips.com>
2020-05-23 15:25:15 +0530

    Repeat a test multiple times using @RepeatedTest and repeat conditionally using RepetitionInfo

--- src/test/java/com/learning/MathUtilsTest.java
+++ src/test/java/com/learning/MathUtilsTest.java
@@ -6,6 +6,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 
 @DisplayName("When running MathUtils")
@@ -33,7 +35,7 @@ class MathUtilsTest {
 		void testAddNegative() {
 			int expected = -10;
 			int actual = mathUtils.add(-10, -20);
-			assertEquals(expected, actual , () -> "should return sum " + expected + " but returned " + actual);
+			assertEquals(expected, actual, () -> "should return sum " + expected + " but returned " + actual);
 		}
 	}
 
@@ -42,10 +44,13 @@ class MathUtilsTest {
 		assertThrows(ArithmeticException.class, () -> mathUtils.divide(10, 0), "Divide by zero should throw");
 	}
 
-	@Test
-	void testCalculateAreaOfCircle() {
-		double result = mathUtils.calculateAreaOfCircle(10);
-		assertEquals(314.1592653589793, result, "The calculateAreaOfCircle method should find area of a circle");
+	@RepeatedTest(3)
+	void testCalculateAreaOfCircle(RepetitionInfo repetitionInfo) {
+		if (1 != repetitionInfo.getCurrentRepetition()) {
+			System.out.println("Calculation area of a circle.");
+			assertEquals(314.1592653589793, mathUtils.calculateAreaOfCircle(10),
+					"The calculateAreaOfCircle method should find area of a circle");
+		}
 	}
 
 }
