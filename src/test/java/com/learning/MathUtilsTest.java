8a393f0505871bab915c5cc84eea2339fd9dcfd3
Bhat <Chinmay.Bhat@philips.com>
2020-05-23 14:53:57 +0530

    Using AsertAll

--- src/test/java/com/learning/MathUtilsTest.java
+++ src/test/java/com/learning/MathUtilsTest.java
@@ -1,13 +1,12 @@
 package com.learning;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
 
 class MathUtilsTest {
 
@@ -19,12 +18,22 @@ class MathUtilsTest {
 	}
 
 	@Test
-	@EnabledOnOs(OS.MAC)//Does not execute the test if pre-condition is not met
 	void testAdd() {
 		int result = mathUtils.add(10, 20);
 		assertEquals(30, result, "The add method should add two numbers");
 	}
 
+	@Test
+	@DisplayName("Multiply method")
+	void testMultiply() {
+		//Test multiple assert statements
+		assertAll(
+				() -> assertEquals(4, mathUtils.multiply(2, 2)),
+				() -> assertEquals(0, mathUtils.multiply(2, 0)),
+				() -> assertEquals(-2, mathUtils.multiply(2, -1))
+		);
+	}
+	
 	@Test
 	void testDivide() {
 		assertThrows(ArithmeticException.class, () -> mathUtils.divide(10, 0), "Divide by zero should throw");
@@ -32,23 +41,8 @@ class MathUtilsTest {
 
 	@Test
 	void testCalculateAreaOfCircle() {
-		int radius = 0;
-		boolean isValidRadius = validateRadius(radius);
-		//Runs the test but skips if the condition is false
-		assumeTrue(isValidRadius, "The radius should be a positive integer");
 		double result = mathUtils.calculateAreaOfCircle(10);
 		assertEquals(314.1592653589793, result, "The calculateAreaOfCircle method should find area of a circle");
-	}
-
-	private boolean validateRadius(int radius) {
-
-		boolean isValid = false;
-
-		if (radius > 0) {
-			isValid = true;
-		}
-
-		return isValid;
 	}
 
 }
